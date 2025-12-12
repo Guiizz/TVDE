c85ab6e254898d2b7261f680b9908f93d98ef9ed
Gui <pereira.gmsp07@gmail.com>
2025-12-12 19:03:14 +0000

    commit cliente - básico

--- src/Model/Cliente.java
+++ src/Model/Cliente.java
@@ -2,78 +2,50 @@ package Model;
 
 public class Cliente {
 
-    private String nome, sexo, nacionalidade;
-    private int idade, id, telemovel;
+    private String nome, morada;
+    private int nif, telemovel;
 
-        /**
-         *
-         * @param nome
-         * @param sexo
-         * @param nacionalidade
-         * @param id
-         * @param idade
-         * @param telemovel
-         */
-
-    public Cliente(String nome, String sexo, String nacionalidade, int id, int idade, int telemovel) {
-            this.nome = nome;
-            this.sexo = sexo;
-            this.nacionalidade = nacionalidade;
-            this.id = id;
-            this.idade = idade;
-            this.telemovel = telemovel;
+    public Cliente(String nome, String morada, int nif, int telemovel) {
+        this.nome = nome;
+        this.morada = morada;
+        this.nif = nif;
+        this.telemovel = telemovel;
     }
 
     public String getNome() {
-            return nome;
+        return nome;
     }
 
     public void setNome(String nome) {
-            this.nome = nome;
+        this.nome = nome;
     }
 
-    public String getSexo() {
-            return sexo;
+    public String getMorada() {
+        return morada;
     }
 
-    public void setSexo(String sexo) {
-            this.sexo = sexo;
+    public void setMorada(String morada) {
+        this.morada = morada;
     }
 
-    public String getNacionalidade() {
-            return nacionalidade;
+    public int getNif() {
+        return nif;
     }
 
-    public void setNacionalidade(String nacionalidade) {
-            this.nacionalidade = nacionalidade;
-    }
-
-    public int getId() {
-            return id;
-    }
-
-    public void setId(int id) {
-            this.id = id;
-    }
-
-    public int getIdade() {
-            return idade;
-    }
-
-    public void setIdade(int idade) {
-            this.idade = idade;
+    public void setNif(int nif) {
+        this.nif = nif;
     }
 
     public int getTelemovel() {
-            return telemovel;
+        return telemovel;
     }
 
     public void setTelemovel(int telemovel) {
-            this.telemovel = telemovel;
+        this.telemovel = telemovel;
     }
 
     @Override
     public String toString(){
-        return "Nome:" + nome + " Idade:" + idade + " Sexo:" + sexo;
+        return "Nome:" + nome + " Nif:" + nif + " Telemóvel:" + telemovel + " Morada:" + morada;
     }
 }
